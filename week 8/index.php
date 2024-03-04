1bb120fbf51e014843addecc49121e8b966edd14
kgjasme <kgjasme@student.apc.edu.ph>
2024-03-04 23:02:06 +0800

    space

--- week 8/index.php
+++ week 8/index.php
@@ -250,7 +250,7 @@
 
 <div class="profile-container">
     <div class="profile-frame">
-        <img src="https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-6/418726352_2916749068464420_8044196981955880205_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=UtjTl6WXIXQAX_T3-33&_nc_ht=scontent.fmnl17-1.fna&oh=00_AfByzU3vzDhK4ywFZlZDrHGLib07JBmNyXGey1C-qH5r1w&oe=65E5366F" alt="Renz" class="profile-image">
+        <img src="https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-6/418726352_2916749068464420_8044196981955880205_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=UtjTl6WXIXQAX_T3-33&_nc_ht=scontent.fmnl17-1.fna&oh=00_AfByzU3vzDhK4ywFZlZDrHGLib07JBmNyXGey1C-qH5r1w&oe=65E5366F"class="profile-image">
     </div>
     <div class="main-heading">
         <h1>Klarenz Jasme</h1>
